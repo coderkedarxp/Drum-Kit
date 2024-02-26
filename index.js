198aee14d89f955ab79ffa7d7ab60d7549430324
coderkedarxp <132534804+coderkedarxp@users.noreply.github.com>
2024-02-26 11:35:53 +0530

    Added Animations

--- index.js
+++ index.js
@@ -34,76 +34,83 @@ var numberofDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberofDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
-
-    
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 // Detecting keypress
 
-document.addEventListener("keypress",function(e){
+document.addEventListener("keypress", function (e) {
   makeSound(e.key);
-})
+  buttonAnimation(e.key);
+});
 
 function makeSound(key) {
-
   switch (key) {
-      case "a":
-        var tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
+    case "a":
+      var tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
 
-      case "s":
-        var tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
+    case "s":
+      var tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
 
-      case "d":
-        var tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
+    case "d":
+      var tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
 
-      case "f":
-        var tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
+    case "f":
+      var tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
 
-      case "j":
-        var snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
+    case "j":
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
 
-      case "k":
-        var crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
+    case "k":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
 
-      case "l":
-        var kick = new Audio("./sounds/kick-bass.mp3");
-        kick.play();
-        break;
+    case "l":
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
 
-      default:
-        console.log(buttonInnerHTML);
-        break;
-    }
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
+}
+
+function buttonAnimation(currentkey) {
+  // to get a ".w" type format that is a class selector
+  var activeButton = document.querySelector("." + currentkey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
 // we passed the parameter e to get to know which event triggered the event listener
 
-
-// NOTE: theory behind  obj creation 
-  // function Audio (fileLocation) {
-  //   this.fileLocation = fileLocation;
-  //   this.play = function(){
-      // tap into file hardware 
-      // check the file at fileLocation exists
-      // check the file at fileLocation file is a sound file
-      // Play the file at fileLocation
-  //   }
-  // }
+// NOTE: theory behind  obj creation
+// function Audio (fileLocation) {
+//   this.fileLocation = fileLocation;
+//   this.play = function(){
+// tap into file hardware
+// check the file at fileLocation exists
+// check the file at fileLocation file is a sound file
+// Play the file at fileLocation
+//   }
+// }
 
 //   var audio = new Audio ("./sounds/tom-1.mp3");
 //     audio.play();
